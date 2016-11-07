2a3beffac76dd99e5700a331f7565488d9f79d1b
elzbietawatroba <elzbietawatroba@UKLOW71485>
2016-11-07 17:53:12 +0000

    Add project files.

---
+++ Ch04_ManipulatingText/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch04_ManipulatingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "Jawor";
+            WriteLine($"{city} is {city.Length} characters long");
+            char first = city[0];
+            WriteLine($"First character is {city[0]}"); // string is array of characters
+            string cities = "Irkuck,Moskwa,Omsk,Perm";
+            string[] citiesArray = cities.Split(','); //split
+            foreach (string item in citiesArray)
+            {
+                WriteLine(item);
+            }
+            string fullname = "Marc Chagall";
+            int space = fullname.IndexOf(' '); // indexof
+            string firstname = fullname.Substring(0, space);
+            string lastname = fullname.Substring(space + 1);
+            WriteLine($"{lastname}, {firstname}");
+            string company = "Abecadlo";
+            bool startswithA = company.StartsWith("A");
+            bool containsN = company.Contains("N");
+            WriteLine($"{company} start with A {startswithA} and contains N {containsN}");
+        }
+    }
+}
---
+++ Ch04_RegularExpressions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+namespace Ch04_RegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter your age");
+            string input = ReadLine();
+            //Regex ageChecker = new Regex(@"^\d+$");
+            Regex ageChecker = new Regex(@"[0-9]{2,}$");
+            if (ageChecker.IsMatch(input))
+            {
+                WriteLine("Thanks");
+            }
+            else
+            {
+                WriteLine($"This is not a valid age: {input}");
+            }
+        }
+    }
+}
