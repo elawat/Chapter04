97ea3a973be2d919c22d953227aa0616328c629f
elzbietawatroba <elzbietawatroba@UKLOW71485>
2016-11-07 18:11:24 +0000

    Lists

---
+++ Ch04_Lists/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch04_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new List<string>(); //<> generics - makes collection strongly types
+            cities.Add("Moskwa");
+            cities.Add("Omsk");
+            cities.Add("Irkuck");
+            WriteLine("Initial Cities");
+            foreach (string city in cities)
+            {
+                WriteLine($"{city}");
+            }
+            WriteLine($"The first city is {cities[0]}.");
+            WriteLine($"The last city is {cities[cities.Count - 1]}");
+            cities.Insert(0, "Perm");
+            WriteLine("After inserting Perm at index 0");
+            foreach (string city in cities)
+            {
+                WriteLine($"{city}");
+            }
+            cities.RemoveAt(1);
+            cities.Remove("Omsk");
+            WriteLine("After removing two cities");
+            foreach (string city in cities)
+            {
+                WriteLine($"{city}");
+            }
+
+
+        }
+    }
+}
