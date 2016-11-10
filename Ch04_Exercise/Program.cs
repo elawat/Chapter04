36dad400f4f67055fc39c222edba299d2159ffbb
elzbietawatroba <elzbietawatroba@UKLOW71485>
2016-11-10 18:36:27 +0000

    Dict and regex exercise

---
+++ Ch04_Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Text.RegularExpressions;
+
+namespace Ch04_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    WriteLine("The default reg exp checks for at least one digit");
+                    WriteLine("Enter reg exp or press ENTER to use the default: ^[a-z]+");
+                    string pattern = ReadLine();
+                    if (pattern == "")
+                    {
+                        pattern = @"^[a-z]+";
+                    }
+                    Regex inputChecker = new Regex(pattern);
+                    WriteLine("Enter some input: ");
+                    string input = ReadLine();
+                    WriteLine($"{input} matches {pattern}? {inputChecker.IsMatch(input)}");
+
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+
+        }
+    }
+}
